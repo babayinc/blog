894b5b7547e5cbeaf2802fd2f0690b311c91f641
Alexander Maksimyak <alexander.max@gmail.com>
2016-07-11 14:53:30 +0300

    Save to db

--- app.rb
+++ app.rb
@@ -14,6 +14,14 @@ before do
 end
 
 configure do
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS
+	Posts
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	 	create_date DATE, 
+	 	content TEXT
+	 )'
 
 end
 
@@ -35,6 +43,10 @@ end
 
 post '/new' do
 	content = params[:content]
-
+	if content.length <= 0
+		@error = 'Type post text'
+		return erb :new
+	end
+	@db.execute 'INSERT INTO Posts (content, create_date) values (?, datetime())', [content]
 	erb "You tiped #{content}"
 end
